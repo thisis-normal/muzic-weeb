0c6e2f2ad4d705b252fc066fa920533ae388b3f4
NTC <nguyenthanhchung.06042002@gmail.com>
2023-10-14 15:31:09 +0700

    add dark mode

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -15,46 +15,49 @@ if (!isset($_SESSION['admin_id'])) {
 
 </head>
 
-<?php require APPROOT . '/views/admin/navbar.php'; ?>
-<section id="content">
-    <nav>
-        <i class="bx bx-menu"></i>
-        <a href="#" class="nav-link">Categories</a>
-        <form action="#">
-            <div class="form-input">
-                <input type="search" placeholder="Search..." />
-                <button type="submit" class="search-btn">
-                    <i class="bx bx-search"></i>
-                </button>
-            </div>
-        </form>
-        <input type="checkbox" id="switch-mode" hidden />
-        <label for="switch-mode" class="switch-mode"></label>
-        <a href="#" class="notification">
-            <i class="bx bxs-bell"></i>
-            <span class="num">8</span>
-        </a>
-        <a href="#" class="profile">
-            <img src="https://scontent.fhan5-11.fna.fbcdn.net/v/t39.30808-6/278388751_364053495635225_4185505353589371366_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=AETREXeIbQIAX8hfEAS&_nc_oc=AQkdERpmXocxc0IYAFYawk_eiyZm8tj4QzMHZgFNdl0tMOKoMXcNl9KWgQ7YHVCBsgw&_nc_ht=scontent.fhan5-11.fna&oh=00_AfBgMhpMh0tH28mpvzyYIcPCTQNW4p2MRQn-A_IFnWSmlg&oe=652FD720" />
-        </a>
-    </nav>
-    <div id="dashboard-tab" class="tab-content active">
-        <?php require APPROOT . '/views/admin/dashboard.php'; ?>
-    </div>
-    <div id="genre-tab" class="tab-content">
-        <?php require APPROOT . '/views/admin/genre.php'; ?>
-    </div>
-    <div id="songrequest-tab" class="tab-content">
-        <?php require APPROOT . '/views/admin/songrequest.php'; ?>
-    </div>
-    <div id="song-tab" class="tab-content">
-        <?php require APPROOT . '/views/admin/song.php'; ?>
-    </div>
-    <div id="artists-tab" class="tab-content">
-        <?php require APPROOT . '/views/admin/artists.php'; ?>
-    </div>
-    <div id="user-tab" class="tab-content">
-        <?php require APPROOT . '/views/admin/user.php'; ?>
-    </div>
-</section>
+<body class="dark">
+    <?php require APPROOT . '/views/admin/navbar.php'; ?>
+    <section id="content">
+        <nav>
+            <i class="bx bx-menu"></i>
+            <a href="#" class="nav-link">Categories</a>
+            <form action="#">
+                <div class="form-input">
+                    <input type="search" placeholder="Search..." />
+                    <button type="submit" class="search-btn">
+                        <i class="bx bx-search"></i>
+                    </button>
+                </div>
+            </form>
+            <input type="checkbox" id="switch-mode" hidden checked />
+            <label for="switch-mode" class="switch-mode"></label>
+            <a href="#" class="notification">
+                <i class="bx bxs-bell"></i>
+                <span class="num">8</span>
+            </a>
+            <a href="#" class="profile">
+                <img src="https://scontent.fhan5-11.fna.fbcdn.net/v/t39.30808-6/278388751_364053495635225_4185505353589371366_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=AETREXeIbQIAX8hfEAS&_nc_oc=AQkdERpmXocxc0IYAFYawk_eiyZm8tj4QzMHZgFNdl0tMOKoMXcNl9KWgQ7YHVCBsgw&_nc_ht=scontent.fhan5-11.fna&oh=00_AfBgMhpMh0tH28mpvzyYIcPCTQNW4p2MRQn-A_IFnWSmlg&oe=652FD720" />
+            </a>
+        </nav>
+        <div id="dashboard-tab" class="tab-content active">
+            <?php require APPROOT . '/views/admin/dashboard.php'; ?>
+        </div>
+        <div id="genre-tab" class="tab-content">
+            <?php require APPROOT . '/views/admin/genre.php'; ?>
+        </div>
+        <div id="songrequest-tab" class="tab-content">
+            <?php require APPROOT . '/views/admin/songrequest.php'; ?>
+        </div>
+        <div id="song-tab" class="tab-content">
+            <?php require APPROOT . '/views/admin/song.php'; ?>
+        </div>
+        <div id="artists-tab" class="tab-content">
+            <?php require APPROOT . '/views/admin/artists.php'; ?>
+        </div>
+        <div id="user-tab" class="tab-content">
+            <?php require APPROOT . '/views/admin/user.php'; ?>
+        </div>
+    </section>
+</body>
+
 <script src="<?= URLROOT ?>/public/js/script.js"></script>
