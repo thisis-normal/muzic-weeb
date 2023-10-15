09415e3f116aa4999fd4e3bea5713628c84e59ce
NTC <nguyenthanhchung.06042002@gmail.com>
2023-10-15 22:46:55 +0700

    Add artist management section

--- app/views/admin/artists.php
+++ app/views/admin/artists.php
@@ -21,7 +21,7 @@
                 </li>
             </ul>
         </div>
-        <a href="#" class="btn-create">
+        <a href="#" class="btn-create btnpopup" data-form="form_create_artist">
             <i class='bx bx-plus'></i>
             <span class="text">Artist Create</span>
         </a>
@@ -56,7 +56,7 @@
 
                         <td>
                             <a href=""><i class='bx bx-trash' style='color:#fb0004'></i></a>
-                            <a href=""><i class='bx bxs-edit' style='color:#0042fb'></i></a>
+                            <a href="" class="edit-button btnpopup" data-form="form_update_artist" data-artistname="Erik" data-biography="6/4/2002" data-image="http://cvcvbnvcxcvbcvxbnvbvzvxbcxvzcvxbc"><i class='bx bxs-edit' style='color:#0042fb'></i></a>
                         </td>
                     </tr>
 
@@ -65,4 +65,66 @@
         </div>
 
     </div>
+    <!-- form create -->
+
+    <form>
+
+        <div class="form_create formAd popup form_create_artist">
+            <h1>Create artist</h1>
+            <br>
+            <div>
+                <input type="text" id="artistname" placeholder="artistname">
+            </div>
+            <div>
+                <textarea name="" id="biography" placeholder="biography" cols="30" rows="10"></textarea>
+
+            </div>
+            <div class="form-controlGroup-inputWrapper">
+                <label class="form-input form-input--file">
+                    <input class="form-input-file" type="file" id="file" accept="image/*" size="14" />
+                    <span class="form-input--file-button">Image</span>
+                    <span class="form-input--file-text">Choose file..</span>
+                </label>
+            </div>
+
+            <div>
+
+                <button id="save-button">artist</button>
+            </div>
+
+        </div>
+    </form>
+    <!-- update form -->
+    <form>
+
+        <div class="form_update formAd popup form_update_artist">
+            <h1>Update artist</h1>
+            <br>
+            <div>
+                <input type="text" id="artistname" data-field="artistname" placeholder="artistname">
+            </div>
+            <div>
+                <textarea name="" id="biography" data-field="biography" placeholder="biography" cols="30" rows="10"></textarea>
+
+            </div>
+
+
+            <div class="form-controlGroup-inputWrapper">
+                <label class="form-input form-input--file file_update">
+                    <input class="form-input-file" type="file" id="file" accept="image/*" size="14" />
+                    <span class="form-input--file-button">Image</span>
+                    <input type="text" class="form-input--file-text" data-field="image">
+                </label>
+            </div>
+
+
+
+
+            <div>
+
+                <button id="save-button">Update artist</button>
+            </div>
+
+        </div>
+    </form>
 </main>
--- app/views/admin/user.php
+++ app/views/admin/user.php
@@ -62,7 +62,7 @@
                 </li>
             </ul>
         </div>
-        <a href="#" class="btn-create user btnpopup">
+        <a href="#" class="btn-create user btnpopup" data-form="form_create_user">
             <i class='bx bx-plus'></i>
             <span class="text">User Create</span>
         </a>
@@ -103,7 +103,7 @@
                         <td>user</td>
                         <td>
                             <a href="" class="delete-user" data-user="Erik"><i class='bx bx-trash' style='color:#fb0004'></i></a>
-                            <a href="" class="edit-button btnpopup" data-user="Erik" data-email="Chungvvvv@gmail.com" data-pass="chung" data-role="Admin"><i class='bx bxs-edit' style='color:#0042fb'></i></a>
+                            <a href="" class="edit-button btnpopup" data-form="form_update_user" data-user="Erik" data-email="Chungvvvv@gmail.com" data-pass="chung" data-role="Admin"><i class='bx bxs-edit' style='color:#0042fb'></i></a>
                         </td>
                     </tr>
 
@@ -114,9 +114,9 @@
     </div>
     <!-- form create -->
 
-    <form id="data-form">
+    <form>
 
-        <div class="form_create popup">
+        <div class="form_create popup form_create_user">
             <h1>Create user</h1>
             <br>
             <div>
@@ -143,31 +143,32 @@
 
         </div>
     </form>
-    <form id="data-form1">
+    <!-- update form -->
+    <form>
 
-        <div class="form_update popup">
+        <div class="form_update popup form_update_user">
             <h1>Update user</h1>
             <br>
             <div>
-                <input type="text" id="username" placeholder="Username">
+                <input type="text" id="username" data-field="user" placeholder="Username">
             </div>
             <div>
 
-                <input type="text" id="email" placeholder="Email">
+                <input type="text" id="email" data-field="email" placeholder="Email">
             </div>
             <div>
 
-                <input type="text" id="password" placeholder="Password">
+                <input type="text" id="password" data-field="pass" placeholder="Password">
             </div>
             <div>
-                <select name="role" id="role">
+                <select name="role" id="role" data-field="role">
                     <option value="User">User</option>
                     <option value="Admin">Admin</option>
                 </select>
             </div>
             <div>
 
-                <button id="save-button">Create Account</button>
+                <button id="save-button">Update Account</button>
             </div>
 
         </div>
