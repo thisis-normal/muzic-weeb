470761679e121fb294362a7fc531ed561021f579
Normal <thuonghuunguyen2002@gmail.com>
2023-10-16 07:24:18 +0700

    updated

--- app/controllers/UserManagement.php
+++ app/controllers/UserManagement.php
@@ -3,4 +3,7 @@ class  Admins extends Controller {
     public function index() {
         $this->view('admin/index'); //load view inside views/admin/index.php
     }
+    public function createUser() {
+
+    }
 }
