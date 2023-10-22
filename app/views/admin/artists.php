cf4a23078890d3cf227bd54e1662a40945be3531
NTC <nguyenthanhchung.06042002@gmail.com>
2023-10-22 18:47:34 +0700

    limit data

--- app/views/admin/artists.php
+++ app/views/admin/artists.php
@@ -36,31 +36,27 @@ require APPROOT . '/views/admin/index.php';
             <div class="order">
                 <table>
                     <thead>
-                    <tr>
-                        <th>Ordinal</th>
-                        <th>Artist name</th>
-                        <th>Biography</th>
-                        <th>Image</th>
-                        <th>Action</th>
-                    </tr>
+                        <tr>
+                            <th>Ordinal</th>
+                            <th>Artist name</th>
+                            <th>Biography</th>
+                            <th style="text-align: center !important;">Image</th>
+                            <th>Action</th>
+                        </tr>
                     </thead>
                     <?php foreach ($data['listArtist'] as $artist) : ?>
-                    <tbody>
-                    <tr>
-                        <td><?= $artist->artist_id ?></td>
-                        <td><?= $artist->name ?></td>
-                        <td><?= $artist->biography ?></td>
-                        <td><img src="<?= IMGROOT ?>/<?= $artist->image ?>" alt="" width="200px"></td>
-                        <td>
-                            <a href="#" class="delete-user" data-delete="<?= $artist->name ?>"
-                               data-delete-href="<?= URLROOT ?>/artist-management/delete-artist/?id=<?= $artist->artist_id ?>"><i
-                                        class='bx bx-trash' style='color:#fb0004'></i></a>
-                            <a href="" class="edit-button btnpopup" data-form="form_update_artist" data-id="<?= $artist->artist_id ?>"
-                               data-artistname="<?= $artist->name ?>" data-biography="<?= $artist->biography ?>"
-                               data-image="<?= $artist->image ?>"><i class='bx bxs-edit' style='color:#0042fb'></i></a>
-                        </td>
-                    </tr>
-                    </tbody>
+                        <tbody>
+                            <tr>
+                                <td><?= $artist->artist_id ?></td>
+                                <td><?= $artist->name ?></td>
+                                <td class="limit-text"><?= $artist->biography ?></td>
+                                <td align="center"><img src="<?= IMGROOT ?>/<?= $artist->image ?>" alt="" width="200px"></td>
+                                <td>
+                                    <a href="#" class="delete-user" data-delete="<?= $artist->name ?>" data-delete-href="<?= URLROOT ?>/artist-management/delete-artist/?id=<?= $artist->artist_id ?>"><i class='bx bx-trash' style='color:#fb0004'></i></a>
+                                    <a href="" class="edit-button btnpopup" data-form="form_update_artist" data-id="<?= $artist->artist_id ?>" data-artistname="<?= $artist->name ?>" data-biography="<?= $artist->biography ?>" data-image="<?= $artist->image ?>"><i class='bx bxs-edit' style='color:#0042fb'></i></a>
+                                </td>
+                            </tr>
+                        </tbody>
                     <?php endforeach; ?>
                 </table>
             </div>
@@ -74,13 +70,11 @@ require APPROOT . '/views/admin/index.php';
                     <input type="text" id="artistname" name="artist_name" placeholder="Artist Name" required>
                 </div>
                 <div>
-                    <textarea name="biography" id="biography" placeholder="Your bio here!" cols="30" rows="10"
-                              required></textarea>
+                    <textarea name="biography" id="biography" placeholder="Your bio here!" cols="30" rows="10" required></textarea>
                 </div>
                 <div class="form-controlGroup-inputWrapper">
                     <label class="form-input form-input--file file_update">
-                        <input class="form-input-file" type="file" id="file" name="image" accept="image/*" size="14"
-                               required/>
+                        <input class="form-input-file" type="file" id="file" name="image" accept="image/*" size="14" required />
                         <span class="form-input--file-button">Image</span>
                         <input type="text" class="form-input--file-text" data-field="image" value="Choose file...">
                     </label>
@@ -96,16 +90,14 @@ require APPROOT . '/views/admin/index.php';
                 <h1>Update artist</h1>
                 <br>
                 <div>
-                    <input type="text" id="artistname" data-field="artistname" name="artist_name"
-                           placeholder="Artist's Name">
+                    <input type="text" id="artistname" data-field="artistname" name="artist_name" placeholder="Artist's Name">
                 </div>
                 <div>
-                    <textarea name="biography" id="biography" data-field="biography" placeholder="Biography" cols="30"
-                              rows="10"></textarea>
+                    <textarea name="biography" id="biography" data-field="biography" placeholder="Biography" cols="30" rows="10"></textarea>
                 </div>
                 <div class="form-controlGroup-inputWrapper">
                     <label class="form-input form-input--file file_update">
-                        <input class="form-input-file" type="file" name="image" id="file" accept="image/*" size="14"/>
+                        <input class="form-input-file" type="file" name="image" id="file" accept="image/*" size="14" />
                         <span class="form-input--file-button">Image</span>
                         <input type="text" class="form-input--file-text" data-field="image">
                     </label>
