1a7bd2feceaf34a02c57cb546dbf13c4fb17b056
NTC <nguyenthanhchung.06042002@gmail.com>
2023-11-09 18:59:28 +0700

    includeFIles

--- app/views/include/includeFiles.php
+++ app/views/include/includeFiles.php
@@ -1,10 +1,18 @@
 <?php
 if (!empty($_SERVER['HTTP_REFERER'])) {
-    // Trang được mở từ yêu cầu AJAX (client-side)
-    // Thực hiện các xử lý tương ứng
-
-    // $url = $_SERVER['REQUEST_URI'];
-    // echo "<script>console.log('$url')</script>";
+    $referer = $_SERVER['HTTP_REFERER'];
+    $currentURL = $_SERVER['REQUEST_URI'];
+    if (strpos($referer, $currentURL) !== false) {
+        require APPROOT . '/views/include/header.php';
+        require APPROOT . '/views/include/navbar.php';
+        require APPROOT . '/views/include/footer.php';
+    } elseif (strpos($referer, URLROOT . '/pages/playlist') !== false) {
+    } elseif (strpos($referer, URLROOT . '/pages/index') !== false) {
+    } else {
+        require APPROOT . '/views/include/header.php';
+        require APPROOT . '/views/include/navbar.php';
+        require APPROOT . '/views/include/footer.php';
+    }
 } else {
     require APPROOT . '/views/include/header.php';
     require APPROOT . '/views/include/navbar.php';
--- app/views/pages/playlist.php
+++ app/views/pages/playlist.php
@@ -5,56 +5,56 @@
 </div>
 <div class="main-container">
     <div id="mainContent">
-        <div class="entityInfo">
+        <div class="spotify-playlists">
 
-            <div class="leftSection">
-                <div class="playlistImage">
-                    <img src="https://th.bing.com/th/id/R.169e29309b53f7873e35844892b3aa64?rik=f4yPfstrpv6L4Q&pid=ImgRaw&r=0">
+
+            <div class="entityInfo">
+
+                <div class="leftSection">
+                    <div class="playlistImage">
+                        <img src="https://th.bing.com/th/id/R.169e29309b53f7873e35844892b3aa64?rik=f4yPfstrpv6L4Q&pid=ImgRaw&r=0">
+                    </div>
                 </div>
+
+                <div class="rightSection">
+                    <h2>a</h2>
+                    <p>By a</p>
+                    <p> songs</p>
+                    <button class="button">DELETE PLAYLIST</button>
+
+                </div>
+
             </div>
+            <table class="tracklist">
+                <thead>
 
-            <div class="rightSection">
-                <h2>a</h2>
-                <p>By a</p>
-                <p> songs</p>
-                <button class="button">DELETE PLAYLIST</button>
+                    <th style="max-width: 30px;">#</th>
+                    <th>Title</th>
+                    <th>Album</th>
+                    <th>Duration</th>
 
-            </div>
+                </thead>
+                <tbody>
+                    <tr class='tracklistRow'>
+                        <td class=''>
+                            1
+                        </td>
+                        <td class='trackCount'>
+                            <img class='play' width="40px" src='https://th.bing.com/th/id/R.169e29309b53f7873e35844892b3aa64?rik=f4yPfstrpv6L4Q&pid=ImgRaw&r=0'>
+                            <div class="">
+                                <span class='trackName'>ANh khasc hay em khasc</span>
+                                <span class='artistName'>abc</span>
+                            </div>
+                        </td>
+                        <td class='trackInfo'>
+                        </td>
+                        <td class='trackDuration'>
+                            <span class='duration'>cccc</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
 
-        </div>
-        <div class="tracklistContainer">
-            <ul class="tracklist">
-
-                <li class='tracklistRow'>
-                    <div class='trackCount'>
-                        <img class='play' src='https://scontent.fhan5-9.fna.fbcdn.net/v/t1.6435-1/132339848_1082217975562826_8163229432639064333_n.jpg?stp=dst-jpg_p100x100&_nc_cat=109&ccb=1-7&_nc_sid=2b6aad&_nc_ohc=xoNptGZIC2kAX8p40Jt&_nc_oc=AQnEi9zY-vjcAMOusj9WcmyuCIULOcvLOP1TzidLm7fa7z37gWtwmygau20qIPi7yHE&_nc_ad=z-m&_nc_cid=1229&_nc_ht=scontent.fhan5-9.fna&oh=00_AfB2mH7ekxI0XHFrgHPwtpQf5v76wC9DJa4UCddqK2tApA&oe=65489C2D'>
-                        <span class='trackNumber'></span>
-                    </div>
-
-
-                    <div class='trackInfo'>
-                        <span class='trackName'>ANh khasc hay em khasc</span>
-                        <span class='artistName'>abc</span>
-                    </div>
-
-                    <div class='trackOptions'>
-                        <input type='hidden' class='songId' value='ssssss'>
-                        <img class='optionsButton' src='assets/images/icons/more.png'>
-                    </div>
-
-                    <div class='trackDuration'>
-                        <span class='duration'>cccc</span>
-                    </div>
-
-
-                </li>
-
-                <script>
-                    var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
-                    tempPlaylist = JSON.parse(tempSongIds);
-                </script>
-
-            </ul>
         </div>
     </div>
 </div>
